d47b8a0f0da514540cc3f4b0cdccd7da4279e06a
rene <rene@linuxkiste.fritz.box>
2025-05-18 21:33:27 +0200

    authentication added

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -8,14 +8,18 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthenticationService } from '../_common/_service/authentication.service';
+import { AlertService } from '../_common/_service/alert.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush, 
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -29,24 +33,57 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  hide = signal(true); 
+  hide = signal(true);
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  isLoading = false;
+
+  loading = false;
+  submitted = false;
+  hideRequiredControl: any;
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
 
-  onLogin() {
-    if (this.loginForm.valid) {
-      console.log('Login Data:', this.loginForm.value);
-      
+  onSubmit() {
+    this.isLoading = true; // Show spinner
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
     }
+
+    this.loading = true;
+    this.authenticationService.login(this.loginForm.controls['email'].value, this.loginForm.controls['password'].value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          // get return url from query parameters or default to home page
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+          this.isLoading = false; // Hide spinner on success
+        },
+        error: (error: any) => {
+          this.alertService.error(error);
+          this.loading = false;
+          this.isLoading = false; // Hide spinner on error
+          this._snackBar.open('Login nicht erfolreich', 'Schließen', { duration: 5000 });
+        }
+      });
   }
-login() {
-  this.router.navigate(['/dashboard']);
-}  
+
+  login() {
+    this.router.navigate(['/dashboard']);
+  }
   togglePassword(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
@@ -54,5 +91,5 @@ login() {
   goToForgotPassword() {
     this.router.navigate(['/forgot-password']);
   }
-  
+
 }
