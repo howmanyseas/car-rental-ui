3c41e2b0f4c1b4877c8b2f6ae8ba18d9f4e2c49d
howmanyseas <sadete.muja@cit.edu.al>
2025-05-19 12:04:55 +0200

    auth for developers

--- frontend/src/app/components/_common/auth.guard.ts
+++ frontend/src/app/components/_common/auth.guard.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from '../_common/_service/authentication.service';
+import { environment } from '../../../environments/environments';
+
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard  {
+export class AuthGuard {
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService
-    ) {}
+    ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (environment.devBypassAuth) {
+            return true;
+        }
         const user = this.authenticationService.userValue;
         if (user) {
             // authorised so return true
@@ -18,7 +23,7 @@ export class AuthGuard  {
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
         return false;
     }
 }
